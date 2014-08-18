30a5b1362f8dcddaf23c7af42e77ef2113d5258d
Sean Reid <sreid757@gmail.com>
2014-08-17 21:24:44 -0500

    changed timer to 19th

--- source/javascripts/soon/custom.js
+++ source/javascripts/soon/custom.js
@@ -6,7 +6,7 @@ COMMING SOON PAGE
     * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
     * according to the GMT+0 Timezone
     **/
-    var launch = new Date(2014, 07, 17, 28, 00);
+    var launch = new Date(2014, 07, 19, 28, 00);
     /**
     * The script
     **/
